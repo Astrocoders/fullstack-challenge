55dbd28a2837ca1aa572c3ee991300b959d9ebd1
jrquadros <jrmquadros@gmail.com>
2019-07-14 00:10:02 -0300

    feat(passport): add config file

---
+++ authServer/config.js
@@ -0,0 +1,5 @@
+exports.TWITTER_CONFIG = {
+    consumerKey: 'EerBlz7imtNCoxFg3JwPPF4kA',
+    consumerSecret: 'HSpgvbrDNWE0CDm0ngiPYY28QjXZuWLuITEFKzAsVknUojUXlq',
+    callbackURL: 'http://localhost:8080/callback'
+}
