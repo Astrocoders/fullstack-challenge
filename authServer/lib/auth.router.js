95daea0f0775cbdb46d984fd82b93cb3b6d798e5
jrquadros <jrmquadros@gmail.com>
2019-07-14 01:12:06 -0300

    feat(express): router

---
+++ authServer/lib/auth.router.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const authController = require('./auth.controller')
+
+const twitterAuth = passport.authenticate('twitter');
+
+router.get('/twitter/callback', twitterAuth, authController.twitter)
+
+router.use((req, res, next) => {
+    req.session.socketId = req.query.socketId
+    next()
+})
+
+router.get('/twitter', twitterAuth)
+
+module.exports = router
