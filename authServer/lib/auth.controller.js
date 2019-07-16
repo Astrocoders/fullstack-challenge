579a171e62b767191c839681413a589d1eb9d6bd
jrquadros <jrmquadros@gmail.com>
2019-07-16 01:22:44 -0300

    fix: remove authServer for use graphqlHub

--- authServer/lib/auth.controller.js
+++
@@ -1,9 +0,0 @@
-exports.twitter = (req, res) => {
-    const io = req.app.get('io')
-    const user = {
-        name: req.user.username,
-        photo: req.user.photos[0].value.replace(/_normal/, '')
-    }
-    io.in(req.session.socketId).emit('twitter', user)
-    res.end()
-}
