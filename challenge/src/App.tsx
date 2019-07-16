f74799a05c202d2a1ac360adc22d554896418de4
jrquadros <jrmquadros@gmail.com>
2019-07-16 19:33:58 -0300

    feat(styled-conponents): global style

--- challenge/src/App.tsx
+++ challenge/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import GlobalStyle from './styles/global'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <GlobalStyle />
       <h1>react app</h1>
     </div>
   );
