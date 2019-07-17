d3ed77cff980cacceecee6b64f59aa4b87ab7dff
jrquadros <jrmquadros@gmail.com>
2019-07-17 00:49:03 -0300

    feat(pages): main page

---
+++ challenge/src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Topbar from '../components/Topbar'
+import Sidebar from "../components/Sidebar";
+
+const Main: React.FC = () => {
+  return <>
+  <Topbar />
+  <Sidebar />
+  </>;
+};
+
+export default Main;
