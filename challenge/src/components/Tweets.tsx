9fdf17f35285441d37664a31357d860a786aabc1
jrquadros <jrmquadros@gmail.com>
2019-07-18 00:39:56 -0300

    feat(apollo):

--- challenge/src/components/Tweets.tsx
+++ challenge/src/components/Tweets.tsx
@@ -1,18 +1,75 @@
 import * as React from "react";
-import styled from 'styled-components'
+import styled from "styled-components";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-const Wrapper = styled.div`
-  right: 0;
-  top: 0;
-  z-index: 999;
-`;
+const Wrapper = styled.div``;
 
 interface Props {
-    query: string;
+  query: string;
 }
 
-const Tweets: React.FC<Props>= ({query}: Props) => {
-  return <><Wrapper><h1>{query}</h1></Wrapper></>;
+interface TwitterUser {
+  name: string;
+}
+
+interface Tweet {
+  id: string;
+  text: string;
+  created_at: string;
+  username: string;
+  user: TwitterUser;
+}
+
+const FETCH_TWEETS = gql`
+  query searchTweets($q: String!) {
+    twitter {
+      search(q: $q, result_type: recent, count: 5) {
+        id
+        text
+        created_at
+        user {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const Tweets: React.FC<Props> = ({ query }: Props) => {
+  const [tweetsList, setTweetsList] = React.useState<Array<Tweet>>([]);
+
+  const { data, loading } = useQuery(FETCH_TWEETS, {
+    variables: { q: query }
+  });
+
+  React.useEffect(() => {
+    const onSearch = () => {
+      setTweetsList(data.twitter.search);
+    };
+
+    if (!loading && data) {
+      onSearch();
+    }
+  }, [data, loading]);
+
+  console.log(tweetsList);
+
+  return (
+    <>
+      <Wrapper>
+        {loading ? (
+          <>loading...</>
+        ) : (
+          <div>
+            {tweetsList.map(t => (
+              <p key={t.id}>{t.text}</p>
+            ))}
+          </div>
+        )}
+      </Wrapper>
+    </>
+  );
 };
 
 export default Tweets;
